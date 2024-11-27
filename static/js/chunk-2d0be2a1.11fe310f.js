ba84563333e4e0a6131dde6043100908d9e4a0f0
Lxg <15674625765@163.com>
2024-11-27 11:28:19 +0800

    11-27

---
+++ static/js/chunk-2d0be2a1.11fe310f.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0be2a1"],{"2ea9":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},r=[],a={data:function(){return{}}},c=a,l=t("2877"),o=Object(l["a"])(c,u,r,!1,null,null,null);n["default"]=o.exports}}]);
